e07759d0b547a0bf50aa3f2f3dbc8b948452b41b
Mathieu Messet <messet@et.esiea.fr>
2019-12-15 06:50:02 +0100

    fixed the view pager and fragment ( crashed on filtering with previous version, still need to sync the scrolls)

--- app/src/main/java/com/example/hearthcards/activities/FragmentMain.java
+++ app/src/main/java/com/example/hearthcards/activities/FragmentMain.java
@@ -33,10 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentMain extends Fragment {
-    private final String JSON_URL = "https://api.hearthstonejson.com/v1/latest/frFR/cards.collectible.json";
-    private JsonArrayRequest request;
-    private RequestQueue requestQueue;
-    private List<Cards> listCards;
+
     private RecyclerView recyclerView;
     View view;
 
@@ -48,121 +45,17 @@ public class FragmentMain extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.main_fragment, container, false);
 
-        listCards = new ArrayList<>();
         recyclerView = view.findViewById(R.id.recycler_view_id);
-        jsonrequest();
 
 
         return view;
     }
 
 
-    public void filter(String text, String classe_filter) {
-        ArrayList<Cards> filteredList = new ArrayList<>();
-
-        for (Cards card : listCards) {
-            if (card.getName().toLowerCase().contains(text.toLowerCase())) {  //|| card.getClasse().toLowerCase().contains(text.toLowerCase()
-
-                if (classe_filter.contains("NO_FILTER")) {
-                    filteredList.add(card);
-                } else if (card.getClasse().contains(classe_filter)) {
-                    filteredList.add(card);
-                }
-            }
-        }
-        setuprecyclerview(filteredList);
-    }
 
 
-    private void jsonrequest() {
-
-        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-
-                JSONObject jsonObject = null;
-                for (int i = 0; i < response.length(); i++) {
-
-                    try {
-                        jsonObject = response.getJSONObject(i);
-                        Cards card = new Cards();
-                        card.setName(jsonObject.getString("name"));
-                        card.setArtist(jsonObject.getString("artist"));
-                        card.setFlavor(jsonObject.getString("flavor"));
-                        card.setId(jsonObject.getString("id"));
-                        card.setClasse(jsonObject.getString("cardClass"));
-                        listCards.add(card);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-                setuprecyclerview(listCards);
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }) {
-            @Override
-            protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
-                try {
-                    Cache.Entry cacheEntry = HttpHeaderParser.parseCacheHeaders(response);
-                    if (cacheEntry == null) {
-                        cacheEntry = new Cache.Entry();
-                    }
-                    final long cacheHitButRefreshed = 3 * 60 * 1000; // in 3 minutes cache will be hit, but also refreshed on background
-                    final long cacheExpired = 24 * 60 * 60 * 1000; // in 24 hours this cache entry expires completely
-                    long now = System.currentTimeMillis();
-                    final long softExpire = now + cacheHitButRefreshed;
-                    final long ttl = now + cacheExpired;
-                    cacheEntry.data = response.data;
-                    cacheEntry.softTtl = softExpire;
-                    cacheEntry.ttl = ttl;
-                    String headerValue;
-                    headerValue = response.headers.get("Date");
-                    if (headerValue != null) {
-                        cacheEntry.serverDate = HttpHeaderParser.parseDateAsEpoch(headerValue);
-                    }
-                    headerValue = response.headers.get("Last-Modified");
-                    if (headerValue != null) {
-                        cacheEntry.lastModified = HttpHeaderParser.parseDateAsEpoch(headerValue);
-                    }
-                    cacheEntry.responseHeaders = response.headers;
-                    final String jsonString = new String(response.data,
-                            HttpHeaderParser.parseCharset(response.headers, "UTF-8"));
-                    return Response.success(new JSONArray(jsonString), cacheEntry);
-                } catch (UnsupportedEncodingException | JSONException e) {
-                    return Response.error(new ParseError(e));
-                }
-            }
-
-            @Override
-            protected void deliverResponse(JSONArray response) {
-                super.deliverResponse(response);
-            }
-
-            @Override
-            public void deliverError(VolleyError error) {
-                super.deliverError(error);
-            }
-
-            @Override
-            protected VolleyError parseNetworkError(VolleyError volleyError) {
-                return super.parseNetworkError(volleyError);
-            }
-        };
 
 
-        requestQueue = Volley.newRequestQueue(getActivity());
-        requestQueue.add(request);
-
-
-    }
 
 
     public void setuprecyclerview(List<Cards> listCards) {
--- app/src/main/java/com/example/hearthcards/activities/MainActivity.java
+++ app/src/main/java/com/example/hearthcards/activities/MainActivity.java
@@ -20,14 +20,35 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import com.android.volley.Cache;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hearthcards.R;
 import com.example.hearthcards.adapters.ViewPagerAdapter;
+import com.example.hearthcards.model.Cards;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-
+    private final String JSON_URL = "https://api.hearthstonejson.com/v1/latest/frFR/cards.collectible.json";
+    private JsonArrayRequest request;
+    private RequestQueue requestQueue;
+    private List<Cards> listCards;
     private DrawerLayout drawer;
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -43,9 +64,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        filter = "NO_FILTER";
 
-        mFragment = (FragmentMain) getSupportFragmentManager().findFragmentByTag("Main");
+        listCards = new ArrayList<>();
+        jsonrequest();
+
+        filter = "NO_FILTER";
 
         tabLayout = findViewById(R.id.tabLayout_id);
         viewPager = findViewById(R.id.viewPager_id);
@@ -70,8 +93,117 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
 
+
     }
 
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        Cards card = new Cards();
+                        card.setName(jsonObject.getString("name"));
+                        card.setArtist(jsonObject.getString("artist"));
+                        card.setFlavor(jsonObject.getString("flavor"));
+                        card.setId(jsonObject.getString("id"));
+                        card.setClasse(jsonObject.getString("cardClass"));
+                        listCards.add(card);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                FragmentMain mFragment = (FragmentMain) viewPager.getAdapter().instantiateItem(viewPager,viewPager.getCurrentItem()) ;
+                mFragment.setuprecyclerview(listCards);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    Cache.Entry cacheEntry = HttpHeaderParser.parseCacheHeaders(response);
+                    if (cacheEntry == null) {
+                        cacheEntry = new Cache.Entry();
+                    }
+                    final long cacheHitButRefreshed = 3 * 60 * 1000; // in 3 minutes cache will be hit, but also refreshed on background
+                    final long cacheExpired = 24 * 60 * 60 * 1000; // in 24 hours this cache entry expires completely
+                    long now = System.currentTimeMillis();
+                    final long softExpire = now + cacheHitButRefreshed;
+                    final long ttl = now + cacheExpired;
+                    cacheEntry.data = response.data;
+                    cacheEntry.softTtl = softExpire;
+                    cacheEntry.ttl = ttl;
+                    String headerValue;
+                    headerValue = response.headers.get("Date");
+                    if (headerValue != null) {
+                        cacheEntry.serverDate = HttpHeaderParser.parseDateAsEpoch(headerValue);
+                    }
+                    headerValue = response.headers.get("Last-Modified");
+                    if (headerValue != null) {
+                        cacheEntry.lastModified = HttpHeaderParser.parseDateAsEpoch(headerValue);
+                    }
+                    cacheEntry.responseHeaders = response.headers;
+                    final String jsonString = new String(response.data,
+                            HttpHeaderParser.parseCharset(response.headers, "UTF-8"));
+                    return Response.success(new JSONArray(jsonString), cacheEntry);
+                } catch (UnsupportedEncodingException | JSONException e) {
+                    return Response.error(new ParseError(e));
+                }
+            }
+
+            @Override
+            protected void deliverResponse(JSONArray response) {
+                super.deliverResponse(response);
+            }
+
+            @Override
+            public void deliverError(VolleyError error) {
+                super.deliverError(error);
+            }
+
+            @Override
+            protected VolleyError parseNetworkError(VolleyError volleyError) {
+                return super.parseNetworkError(volleyError);
+            }
+        };
+
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+
+    }
+
+
+
+    private void filter(String text, String classe_filter) {
+        ArrayList<Cards> filteredList = new ArrayList<>();
+
+        for (Cards card : listCards) {
+            if (card.getName().toLowerCase().contains(text.toLowerCase())) {  //|| card.getClasse().toLowerCase().contains(text.toLowerCase()
+
+                if (classe_filter.contains("NO_FILTER")) {
+                    filteredList.add(card);
+                } else if (card.getClasse().contains(classe_filter)) {
+                    filteredList.add(card);
+                }
+            }
+        }
+        FragmentMain mFragment = (FragmentMain) viewPager.getAdapter().instantiateItem(viewPager,viewPager.getCurrentItem()) ;
+        mFragment.setuprecyclerview(filteredList);
+    }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -104,7 +236,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public boolean onQueryTextChange(String newText) {
 
-                mFragment.filter(newText, filter);
+                filter(newText, filter);
                 return false;
             }
         });
@@ -161,10 +293,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             filter = newFilter;
             menuItem.setChecked(true);
         }
-        mFragment.filter("", filter);
+        filter("", filter);
 
         return true;
     }
+
 
     @Override
     public void onBackPressed() {
